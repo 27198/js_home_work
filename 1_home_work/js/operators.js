602e3740b2c8a3fd2a3a1b201b5f04ee9cdce800
Alexander Sukhanov <ceo@petroflot.com>
2022-03-29 09:23:22 +0300

    1_home_work

---
+++ 1_home_work/js/operators.js
@@ -0,0 +1,16 @@
+'use strict'; 
+
+let price = 1001;
+let a = 1000; 
+let result = price > a ? price * 0.9 : price;
+console.log(result); 
+
+let number = 45;
+let ab = number%10;
+let aa = Math.floor(number / 10);
+let total = ab + aa;
+console.log(total);
+
+let x = 3;
+let y = 7;
+console.log(x*y/x, y*x/y);
